d6a3431d6b76aecf4617d3085431bb9a68a82e7b
Diya <79692259+diya24-d@users.noreply.github.com>
2022-05-29 11:25:02 +0530

    Add files via upload

---
+++ DSassignment4.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+  
+sparseMatrix = csr_matrix((3, 4), 
+                          dtype = np.int8).toarray()
+  
+print(sparseMatrix)
+row = np.array([0, 0, 1, 1, 2, 1])
+col = np.array([0, 1, 2, 0, 2, 2])
+  
+data = np.array([1, 4, 5, 8, 9, 6])
+  
+sparseMatrix = csr_matrix((data, (row, col)), 
+                          shape = (3, 3)).toarray()
+
+    
+print(sparseMatrix)
